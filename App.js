6dc6988b660db8b59abb0a155babbe3a22d295e1
Waltteri <waltteri.toivonen@student.hamk.fi>
2020-09-29 12:04:13 +0300

    waltter

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Tässä on Kallen lisäämä teksti!</Text>
+      <Text>Tässä on Waltterin lisäämä teksti!</Text>
       <StatusBar style="auto" />
     </View>
   );
