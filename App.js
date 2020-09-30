f033dfb0dfeddafe007e18167728fc561f844cda
sundeka <sundellkalle@gmail.com>
2020-09-30 14:38:56 +0300

    Etusivun front end

--- App.js
+++ App.js
@@ -1,21 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
+
+import FrontPage from './pages/FrontPage.js'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Testitestitesti</Text>
-      <StatusBar style="auto" />
-    </View>
+    <FrontPage/>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
