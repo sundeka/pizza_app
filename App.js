1fbc7e53ea12babee044ed8cd51031815ab595e0
sundeka <sundellkalle@gmail.com>
2020-09-30 10:26:16 +0300

    Kansioiden lisäys

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Branchmuokkaustesti.</Text>
+      <Text>Testitestitesti</Text>
       <StatusBar style="auto" />
     </View>
   );
